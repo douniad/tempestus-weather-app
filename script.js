770ae010bb590b4ab26effeb734bbb9d8d48035d
Dounia Dif <douniadif@outlook.com>
2020-10-08 14:38:50 +0200

    fixed bugs

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const appid = "f545988442b3d003b6afda2af5c926c7"
 const searchURL = 'https://api.openweathermap.org/data/2.5/weather' 
 
-let res 
+const allCities = []
 
 function formatQueryParams(params) {
 const queryItems = Object.keys(params)
@@ -21,15 +21,28 @@ function getWeather(q) {
  const url = searchURL+ '?' + queryString;
 
 
+let res
 
  return fetch(url)
  .then(response =>  {
    res = response
+   if (!response.ok) {
+  
+     const message = response.status===404? `${response.status} - Please search for a valid city!`: "Sorry, something went wrong. Try again later."
+     throw new Error (message)
+   }
+   
+   allCities.push(q)
    return response.json()
-  })    
+  })  
+  .catch( error => {
+    const message = res ? error.message: "Sorry, something went wrong. Try again later."
+   console.log(message);
+    $("#js-error-message").text(message)
+  })  
 }
 
-const allCities = []
+
 
 function watchForm() {
   $('form').submit(event => {
@@ -40,15 +53,13 @@ function watchForm() {
      $("#js-error-message").text(`You already know the weather for ${cityName}`)
      return 
     }
-    allCities.push(cityName)
+   
     $("#js-error-message").text("");
     $("#js-search-term").val("");
     $("js-search-term").focus();
     getWeather(cityName)
     .then(displayWeather)
-    .catch(error => {
-      $('#js-error-message').text(`${res.status} - Please search for a valid city!`);
-    })
+    
   })
 }
 
